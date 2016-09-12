679c44f0285624879c9df1ffaa0641d2fd5c1cfa
林嘉豪 <1143910315@qq.com>
2016-09-12 22:23:59 +0800

    格式调整

--- src/控制端/Kongzhi.java
+++ src/控制端/Kongzhi.java
@@ -2,7 +2,6 @@ package 控制端;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
@@ -10,36 +9,34 @@ public class Kongzhi implements MouseListener {
 	private JButton		aJButton;
 	private JTextField	aJTextField;
 	private String		IP;
-	
+
 	public Kongzhi(JButton aJButton, JTextField aJTextField) {
 		this.aJButton = aJButton;
 		this.aJTextField = aJTextField;
 	}
-
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if (aJButton.getText() == "连接") {
 			IP = aJTextField.getText();
 			aJButton.setText("停止");
 		} else {
-
 		}
 	}
-	
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 	}
-	
+
 	@Override
 	public void mouseExited(MouseEvent e) {
 	}
-	
+
 	@Override
 	public void mousePressed(MouseEvent e) {
 	}
-	
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
 	}
-	
 }
